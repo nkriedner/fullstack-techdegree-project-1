0a3ecf3fe337074e9354904c350b77647fa2d371
Nils Kriedner <nkriedner@googlemail.com>
2021-06-21 09:18:37 +0200

    Create an array with quote objects

--- js/script.js
+++ js/script.js
@@ -10,8 +10,45 @@ project 1 - A Random Quote Generator
 /*** 
  * `quotes` array 
 ***/
-
-
+const quotes = [
+  {
+    quote: "Follow the love in your heart - not the stress in your head.",
+    source: "Pari"
+  },
+  {
+    quote: "Modern people can't see god because they don't look low enough.",
+    source: "Carl Jung"
+  },
+  {
+    quote: "A teacher has answers to question. A coach has questions to answers.",
+    source: "Arjuna Ardagh"
+  },
+  {
+    quote: "The key to freedom? End every judgment with a question mark.",
+    source: "Byron Katie"
+  },
+  {
+    quote: "The things that matter most should never be at the mercy of the things that matter least.",
+    source: "Johann Wolfgang von Goethe"
+  },
+  {
+    quote: "Simplicity is the ultimate sophistication.",
+    source: "Leonardo da Vinci"
+  },
+  {
+    quote: "Set your life on fire. Seek those who fan your flames.",
+    source: "Rumi"
+  },
+  {
+    quote: "Always, always seek mystery. Small minds sit in answers. Great minds sit in questions.",
+    source: "Zan Perrion"
+  },
+  {
+    quote: "Wealth is the ability to fully experience life.",
+    source: "Thoreau"
+  }
+];
+console.log(quotes);
 
 /***
  * `getRandomQuote` function
